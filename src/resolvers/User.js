d7ce74db5eda15c46af555ab85490f640894691c
Neword <newordn@gmail.com>
2020-06-22 17:40:10 -0700

    updated listing order of quarantines inside User

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,5 +1,5 @@
 const quarantaines= async (parent,args,context,info)=>{
-    const quarantaines = await context.prisma.user({id:parent.id}).quarantaines()
+    const quarantaines = await context.prisma.user({id:parent.id}).quarantaines({orderBy:'id_DESC'})
     return quarantaines
 }
 
